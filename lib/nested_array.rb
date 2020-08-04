420622477c6f1f14c7efff523f6be0ac9f43c2b1
Farhanul Karim <farhanulkarim3770@gmail.com>
2020-08-04 21:37:45 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -36,7 +36,7 @@ end
   # Using Array literal syntax only, build a nested array that uses the data in
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
-  sortedMatrix = [CONVENTIONAL_PRODUCE.sort, ORGANIC_PRODUCE.sort]
+  sortedMatrix = [ORGANIC_PRODUCE.sort, CONVENTIONAL_PRODUCE.sort]
 end 
 
 
